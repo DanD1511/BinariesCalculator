a6695af696f83a6f3f9d0fbbba2ab2d99bec56dc
DanD1511 <ddacunha012001@gmail.com>
2023-04-16 16:40:34 -0500

    BinToDec working

--- app/src/main/java/com/dand0129/binariescalculator/MainActivity.kt
+++ app/src/main/java/com/dand0129/binariescalculator/MainActivity.kt
@@ -1,49 +1,57 @@
 package com.dand0129.binariescalculator
 
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Input
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
-import com.dand0129.binariescalculator.ui.theme.BinariesCalculatorTheme
 
 class MainActivity : ComponentActivity() {
+    val viewModel: ViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PrincipalScreen()
+            PrincipalScreen(viewModel)
         }
     }
 }
 
 @Composable
-private fun PrincipalScreen() {
+private fun PrincipalScreen(viewModel: ViewModel) {
     var shouldShowScreen by rememberSaveable { mutableStateOf<Boolean>(true) }
     var shouldShowScreen2 by rememberSaveable { mutableStateOf<Boolean>(true) }
 
     Surface {
         if (shouldShowScreen && shouldShowScreen2) {
-            WelcomeScreen(onContinueClicked1 = { shouldShowScreen = false },
-                onContinueClicked2 = { shouldShowScreen2 = false})
-        } else if (!shouldShowScreen && shouldShowScreen2) {
-            BinToDec()
-        } else if (shouldShowScreen && !shouldShowScreen2) {
-            DecToBin()
+            WelcomeScreen(
+                onContinueClicked1 = { shouldShowScreen = false },
+                onContinueClicked2 = { shouldShowScreen2 = false }
+            )
+        } else if (!shouldShowScreen) {
+            BinToDec(viewModel)
+        } else if (!shouldShowScreen2) {
+            DecToBin(viewModel)
         } else return@Surface
     }
 }
@@ -57,23 +65,73 @@ fun WelcomeScreen(
         Button(onClick = {
             onContinueClicked1()
         }) {
-            Text(text = "Binario a decimal")
+            Text(text = "Bin to Dec")
         }
         Spacer(modifier = Modifier.height(10.dp))
         Button(onClick = {
             onContinueClicked2()
         }) {
-            Text(text = "Decimal a binario")
+            Text(text = "Dec to Bin")
         }
     }
 }
 
 @Composable
-fun BinToDec() {
-    Text(text = "Binario a decimal")
+fun BinToDec(viewModel: ViewModel) {
+    val textToShow by viewModel.textToShow.observeAsState()
+
+    Column() {
+        InputNumber(
+            title = "Bin To Dec",
+            onButtonClicked = { value ->
+                viewModel.binToDec(value)
+            }
+        )
+        Text(
+            text = textToShow!!
+        )
+    }
 }
 
 @Composable
-fun DecToBin() {
-    Text(text = "Decimal a binario")
+fun DecToBin(viewModel: ViewModel) {
+    val textToShow by viewModel.textToShow.observeAsState()
+
+    Column() {
+        InputNumber(
+            title = "Dec to Bin",
+            onButtonClicked = { value ->
+                viewModel.decToBin(value)
+            }
+        )
+        Text(
+            text = textToShow!!
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InputNumber(
+    title: String = "title",
+    onButtonClicked: (String) -> Unit
+) {
+    val inputNumber = remember { mutableStateOf("") }
+
+    Column() {
+        Text(text = title)
+        TextField(
+            value = inputNumber.value,
+            onValueChange = {
+                inputNumber.value = it
+            }
+        )
+        Button(
+            onClick = {
+                onButtonClicked(inputNumber.value)
+            }
+        ) {
+
+        }
+    }
 }
---
+++ app/src/main/java/com/dand0129/binariescalculator/ViewModel.kt
@@ -0,0 +1,35 @@
+package com.dand0129.binariescalculator
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ViewModel: ViewModel() {
+
+    private val _number1 = MutableLiveData("")
+    val number1: LiveData <String> get() = _number1
+
+    private val _result = MutableLiveData(0)
+    val result: LiveData <Int> get() = _result
+
+    private val _textToShow = MutableLiveData("")
+    val textToShow: LiveData <String> get() = _textToShow
+
+    fun binToDec(number: String) {
+        if(isBinary(number)){
+            _result.value = number.toInt(2)
+            _textToShow.value = _result.value.toString()
+        } else{
+            _textToShow.value = "Please enter a binary number"
+        }
+    }
+
+     private fun isBinary(number: String): Boolean {
+        val pattern = Regex("^[01]+$")
+        return pattern.matches(number)
+    }
+
+    fun decToBin(number: String) {
+
+    }
+}
